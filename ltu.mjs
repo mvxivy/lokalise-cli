63f7a668e2ccdf04960894ea64ef36d77f1b9c74
Maksim Ivanov <vonavimax@gmail.com>
2025-06-07 22:30:55 +0700

    refactor: decompose fetch locales and ltu

--- ltu.mjs
+++ ltu.mjs
@@ -1,4 +1,18 @@
 #!/usr/bin/env node
-import { fetchLocales } from "./fetch-locales.mjs";
+import { LokaliseApi } from '@lokalise/node-api';
+import { fetchLocales } from "./lib/fetch-locales.mjs";
+import { getConfigFromFile } from './lib/config.mjs';
+import { branchProcess } from './lib/branch-process.mjs';
 
-fetchLocales();
+async function main(config) {
+  if (!config) {
+    config = await getConfigFromFile();
+  }
+
+  const lokalise = new LokaliseApi({ apiKey: config.apiKey });
+
+  await branchProcess(lokalise, config);
+  await fetchLocales(lokalise, config);
+}
+
+main();
