3a5ccad88a2188c616f7cc2d0999b3a11e91ff35
Maksim Ivanov <vonavimax@gmail.com>
2025-06-17 13:25:52 +0700

    feat: graceful shutdown

--- ltu.mjs
+++ ltu.mjs
@@ -3,6 +3,7 @@ import { LokaliseApi } from '@lokalise/node-api';
 import { fetchLocales } from "./lib/fetch-locales.mjs";
 import { getConfigFromFile } from './lib/config.mjs';
 import { branchProcess } from './lib/branch-process.mjs';
+import process from 'node:process';
 
 async function main(config) {
   if (!config) {
@@ -16,3 +17,12 @@ async function main(config) {
 }
 
 main();
+
+function gracefulShutdown(error) {
+  setTimeout(() => process.exit(error ? 1 : 0), 100);
+}
+
+process.on('uncaughtException', gracefulShutdown);
+process.on('unhandledRejection', gracefulShutdown);
+process.on('SIGINT', () => gracefulShutdown());
+process.on('SIGTERM', () => gracefulShutdown());
